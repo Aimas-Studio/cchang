d6f10899482d88b1a8d2bf6fc7e67ed9b60bc4bf
Marian <mariansusanaas@gmail.com>
2023-11-09 23:03:45 -0500

    textos agregados

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -4,6 +4,7 @@ import UnderlinedNavBar from "./components/navbar";
 import UnderlinedSingleNavBar from "./components/single-navbar";
 import HeaderText from "./components/header-text";
 import "./css/inicio.css";
+import OnlyText from "./components/onlytext";
 
 function Inicio() {
   return (
@@ -13,10 +14,22 @@ function Inicio() {
         <div className="columns-container">
           <div className="col-6ymedio">
             <UnderlinedNavBar />
+            
+            <OnlyText
+              text = 'Contamos con una fuerza de trabajo calificada y enfoca su gestión en lograr instalaciones confortables y sostenibles.'
+            />
+            
             <HeaderText
-              header="Misión"
+              header="MISIÓN"
               text="Construir, reparar y mantener infraestructuras, así como instalar y sostener sistemas eléctricos y de refrigeración y clima, garantizando calidad y confort; con un personal competente y comprometido con satisfacer las exigencias de los clientes."
             />
+
+            <HeaderText
+              header="VISIÓN"
+              text="Contamos con el respeto y fidelidad de nuestros clientes, internos y externos, que apuestan por la calidad, excelencia y garantía de nuestros servicios."
+            />
+
+
           </div>
           <div className="col-0">
 
