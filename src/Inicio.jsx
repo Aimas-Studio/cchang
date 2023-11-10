2d33349b3e0cd5a8320fd338e3fa5e0e1369b182
Marian <mariansusanaas@gmail.com>
2023-11-10 00:59:33 -0500

    first commit

---
+++ src/Inicio.jsx
@@ -0,0 +1,13 @@
+import ImageHeader from "./components/image-header";
+import Contacto from "./components/contacto";
+
+function Inicio() {
+    return (
+    <div className="inicio-page-container">
+
+    <ImageHeader/>
+    <Contacto/>    
+    </div>
+    );   
+}
+export default Inicio;
